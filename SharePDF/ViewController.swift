74cbd5b8e04f0dec5d13d639a047f661df667ca9
Olga Zhidkova <zhidkova.olga12@gmail.com>
2022-04-22 10:56:01 +0300

    WebKit

--- SharePDF/ViewController.swift
+++ SharePDF/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import WebKit
+import PDFKit
 
 class ViewController: UIViewController {
-
+    
+    let url = URL(string: "https://developers.apple.com/support/downloads/terms/apple-developer-program/Apple-Developer-Program-License-Agreement-20210607-Russian.pdf")!
+    
+    var pdf: PDFDocument!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBAction func shareButtonAction(_ sender: Any) {
+        let shareController = UIActivityViewController(activityItems: [pdf.dataRepresentation() as Any], applicationActivities: nil)
+        present(shareController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webView.load(URLRequest(url: url))
+        pdf = PDFDocument(url: url)
     }
-
-
 }
 
